e6ecaed3782275d647229ac98b26a820615dcf3c
EnzoClares <enzoclares1712@gmail.com>
2025-07-07 18:42:37 -0300

    Criando pasta html

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) ##Referência ao próprio arquivo(garante com que o código rode)
 
 @app.route('/inicio')
 def ola():
-  return "<h1> Olá Mundo </h1>"
+  return render_template("lista.html")
 
 app.run() ##Roda a aplicação
 
